688f42a946b9cbcaf47a5bfffa9f85f44e67e53e
Anjali Nuggehalli <114877298+anjalinugg4@users.noreply.github.com>
2025-05-12 11:30:45 -0700

    Update WeatherCNN.py

--- WeatherCNN.py
+++ WeatherCNN.py
@@ -38,21 +38,31 @@ transform = transforms.Compose([
 dataset = ImageFolder(data_dir, transform=transform)
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-# 5. Simple CNN Model
+
 class WeatherCNN(nn.Module):
     def __init__(self, num_classes):
         super(WeatherCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
-        self.fc1 = nn.Linear(32 * 32 * 32, 128)
-        self.fc2 = nn.Linear(128, num_classes)
+        self.dropout = nn.Dropout(0.5)
+        
+        self.fc1 = nn.Linear(128 * 16 * 16, 256)  # Input size changes due to 3 poolings
+        self.fc2 = nn.Linear(256, num_classes)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))  # [B, 16, 64, 64]
-        x = self.pool(F.relu(self.conv2(x)))  # [B, 32, 32, 32]
-        x = x.view(-1, 32 * 32 * 32)
-        x = F.relu(self.fc1(x))
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))  # -> [B, 32, 64, 64]
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))  # -> [B, 64, 32, 32]
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))  # -> [B, 128, 16, 16]
+        x = x.view(-1, 128 * 16 * 16)
+        x = self.dropout(F.relu(self.fc1(x)))
         return self.fc2(x)
 
 # 6. Training Setup
